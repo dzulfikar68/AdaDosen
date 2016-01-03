44876ac267ebd4213118f13c15e5ce1800a0b0fd
Faqih Arifian Ajipradana <faqiharifianaji@gmail.com>
2016-01-03 18:26:08 +0700

    modify friendly date, add static parse lecturer's json method

--- app/src/main/java/com/digitcreativestudio/safian/adadosen/Utils/Utils.java
+++ app/src/main/java/com/digitcreativestudio/safian/adadosen/Utils/Utils.java
@@ -1,7 +1,16 @@
 package com.digitcreativestudio.safian.adadosen.Utils;
 
+import android.content.ContentValues;
+
+import com.digitcreativestudio.safian.adadosen.Data.DBContract;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by faqih_000 on 1/1/2016.
@@ -23,13 +32,13 @@ public class Utils {
             if(diff / (24*3600000) < 2)
                 result = "Kemarin";
             else
-                result = Math.round((float) (diff / (24*60*60*1000))) +" hari yang lalu";
+                result = Math.round(Math.ceil(diff / (24*60*60*1000))) +" hari yang lalu";
         }else if(diff / (60*1000) > 59){
-            result = Math.round((float) (diff / (60*60*1000))) +" jam yang lalu";
+            result = Math.round(Math.ceil(diff / (60*60*1000))) +" jam yang lalu";
         }else if(diff / (1000) > 59){
-            result = Math.round((float) (diff / (60*1000))) +" menit yang lalu";
+            result = Math.round(Math.ceil(diff / (60*1000))) +" menit yang lalu";
         }else if(diff / (1000) > 0){
-            result = Math.round((float) (diff / (1000))) +" detik yang lalu";
+            result = Math.round(Math.ceil(diff / (1000))) +" detik yang lalu";
         }else{
             result = "Baru saja";
         }
@@ -56,5 +65,39 @@ public class Utils {
 //        }
 
         return result;
+    }
+
+    public static String shortenName(String name){
+        String[] nameArray = name.split("\\s+");
+
+        String newName = "";
+        for(int i = 0; i < nameArray.length; i++){
+            if(i>1){
+                newName += " "+nameArray[i].charAt(0)+".";
+            }else{
+                newName += " "+nameArray[i];
+            }
+        }
+        return newName;
+    }
+
+    public static ContentValues parseJsonLecturer(JSONObject jsonLecturer){
+        ContentValues cv = new ContentValues();
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+            cv.put(DBContract.LecturerEntry._ID, jsonLecturer.getInt("id"));
+            cv.put(DBContract.LecturerEntry.COLUMN_NIP, jsonLecturer.getString("nip"));
+            cv.put(DBContract.LecturerEntry.COLUMN_NAME, jsonLecturer.getString("name"));
+            cv.put(DBContract.LecturerEntry.COLUMN_STATUS, jsonLecturer.getString("status").equals("1"));
+            cv.put(DBContract.LecturerEntry.COLUMN_LAST_MODIFY, sdf.parse(jsonLecturer.getString("last_modify")).getTime());
+            cv.put(DBContract.LecturerEntry.COLUMN_MODIFIED_BY, shortenName(jsonLecturer.getString("modified_by")));
+        }catch (JSONException je){
+            je.printStackTrace();
+        }catch(ParseException pe){
+            pe.printStackTrace();
+        }
+        return cv;
     }
 }
